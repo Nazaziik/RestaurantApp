6fcdf511c36fcc749488baa4d9c8b649454cba43
Nazarii Shchehliuk <nazar240902@gmail.com>
2023-04-14 12:53:53 +0200

    Specification implementing

--- Domain/Interfaces/IGenericRepository.cs
+++ Domain/Interfaces/IGenericRepository.cs
@@ -7,5 +7,9 @@ namespace Domain.Interfaces
         Task<T> GetByIdAsync(int id);
 
         Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetEntityWithSpecAsync(ISpecification<T> specification);
+
+        Task<IReadOnlyList<T>> GetAllWithSpecAsync(ISpecification<T> specification);
     }
 }
