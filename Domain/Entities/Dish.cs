8a52017a49ba073b452736010bbeb1dbe3352ab4
Nazarii Shchehliuk <nazar240902@gmail.com>
2023-08-19 20:08:13 -0500

    Order by price imp + order by name problem

--- Domain/Entities/Dish.cs
+++ Domain/Entities/Dish.cs
@@ -1,7 +1,11 @@
-﻿namespace Domain.Entities
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
 {
     public class Dish : BaseEntity
     {
+        //[Column(TypeName = "varchar(MAX)")]  // This should fix ordering problem
         public string Name { get; set; }
 
         public string Description { get; set; }
--- Domain/Specifications/DishWithTypeAndProductsSpec.cs
+++ Domain/Specifications/DishWithTypeAndProductsSpec.cs
@@ -3,15 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Specifications
 {
-    public class DishWithTypeAndProductsSpec: BaseSpecification<Dish>
+    public class DishWithTypeAndProductsSpec : BaseSpecification<Dish>
     {
-        public DishWithTypeAndProductsSpec()
+        public DishWithTypeAndProductsSpec(string sort)
         {
-            AddMultipleIncludes(q => q.Include(d  => d.Type));
+            AddMultipleIncludes(q => q.Include(d => d.Type));
             AddMultipleIncludes(q => q.Include(d => d.Products).ThenInclude(p => p.Type));
+            AddOrderBy(d => d.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(d => d.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(d => d.Price);
+                        break;
+                    default:
+                        AddOrderBy(d => d.Name);
+                        break;
+                }
+            }
         }
 
-        public DishWithTypeAndProductsSpec(int id): base(d => d.Id == id)
+        public DishWithTypeAndProductsSpec(int id) : base(d => d.Id == id)
         {
             AddInclude(d => d.Type);
             AddInclude(d => d.Products);
